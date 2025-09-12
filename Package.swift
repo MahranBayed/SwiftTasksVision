e20e175ac24f3e80301225474c9c690076499ff8
pascalburlet <pburlet@outlook.com>
2025-09-12 22:48:26 +0200

    Initial commit with first release frameworks and manual release

---
+++ Package.swift
@@ -0,0 +1,56 @@
+// swift-tools-version:5.9
+import PackageDescription
+import Foundation
+
+let package = Package(
+    name: "SwiftTasksVision",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "SwiftTasksVision",
+            targets: ["SwiftTasksVision"]
+        ),
+        .executable(
+            name: "UpdatePackage",
+            targets: ["UpdatePackage"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "SwiftTasksVision",
+            dependencies: ["SwiftTasksVisionCore"]
+        ),
+        .target(
+            name: "SwiftTasksVisionCore",
+            dependencies: ["MediaPipeCommonGraphLibraries", "MediaPipeTasksVision"],
+            linkerSettings: [
+                .unsafeFlags(["-ObjC"]),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("CoreVideo"),
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("Accelerate"),
+                .linkedLibrary("c++")
+            ]
+        ),
+        .binaryTarget(
+            name: "MediaPipeTasksVision",
+            path: "Dependencies/MediaPipeTasksVision.xcframework"
+        ),
+        .binaryTarget(
+            name: "MediaPipeCommonGraphLibraries",
+            path: "Dependencies/MediaPipeCommonGraphLibraries.xcframework"
+        ),
+        .binaryTarget(
+            name: "MediaPipeTasksCommon",
+            path: "Dependencies/MediaPipeTasksCommon.xcframework"
+        ),
+        .executableTarget(
+            name: "UpdatePackage",
+            resources: [
+                .process("Resources/MediaPipeVision.Info.plist")
+            ]
+        ),
+    ]
+)
